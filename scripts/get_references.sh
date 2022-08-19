a2db6bfc017973ab88e9c93ed03ca312f97644dc
laura.lwd <laura.w.dijkhuizen@gmail.com>
2022-08-19 14:21:22 +0200

    correct ftp to http paths

--- scripts/get_references.sh
+++ scripts/get_references.sh
@@ -4,11 +4,11 @@ then wget 'https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_000196515.1?download=
 fi
 
 if   [ ! -f references/Azfil_v1.2.fasta ]
-then  wget ftp://ftp.fernbase.org/Azolla_filiculoides/Azolla_asm_v1.1/Azolla_filiculoides.genome_v1.2.fasta \
+then  wget https://fernbase.org/ftp/Azolla_filiculoides/Azolla_asm_v1.1/Azolla_filiculoides.genome_v1.2.fasta \
            -O references_Azfil_v1.fasta
 fi
 
 if   [ ! -f references/Azfil_cp1.4.fasta ]
-then  wget ftp://ftp.fernbase.org/Azolla_filiculoides/Azolla_asm_v1.1/chloroplast_genome/Azolla_filiculoides.cp_genome_v1_4.fasta
+then  wget https://fernbase.org/ftp/Azolla_filiculoides/Azolla_asm_v1.1/chloroplast_genome/Azolla_filiculoides.cp_genome_v1_4.fasta \
            -O references/Azfil_cp1.4.fasta
 fi
