eefcdeefd534e736ff51f01dce0ce13be5cabc10
laura.lwd <laura.w.dijkhuizen@gmail.com>
2022-08-19 13:45:12 +0200

    make script for getting published reference fastas

---
+++ scripts/get_references.sh
@@ -0,0 +1,14 @@
+if   [ ! -f references/Nazollae_0708.fasta ]
+then wget 'https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_000196515.1?download=true&gzip=false'\
+         -O references/Nazollae_0708.fasta
+fi
+
+if   [ ! -f references/Azfil_v1.2.fasta ]
+then  wget ftp://ftp.fernbase.org/Azolla_filiculoides/Azolla_asm_v1.1/Azolla_filiculoides.genome_v1.2.fasta \
+           -O references_Azfil_v1.fasta
+fi
+
+if   [ ! -f references/Azfil_cp1.4.fasta ]
+then  wget ftp://ftp.fernbase.org/Azolla_filiculoides/Azolla_asm_v1.1/chloroplast_genome/Azolla_filiculoides.cp_genome_v1_4.fasta
+           -O references/Azfil_cp1.4.fasta
+fi
