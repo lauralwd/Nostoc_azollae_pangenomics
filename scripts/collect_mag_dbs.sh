ee82384cd8a807965fa9653e51b42e84c08ffb19
laura.lwd <laura.w.dijkhuizen@gmail.com>
2022-08-25 11:22:10 +0200

    match sample naming between profile and contig dbs

--- scripts/collect_mag_dbs.sh
+++ scripts/collect_mag_dbs.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 mkdir  data/MAG_anvi_dbs/ 2> /dev/null
 # get contigdbs
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_hybrid_doublefiltered_anvio/Azfil_wild/Azfil_wild_contigs.db		             data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_hybrid_doublefiltered_anvio/Azfil_lab/Azfil_lab_contigs.db		               data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azrub_IRRI_479/Azrub_IRRI_479_contigs.db		     data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azmex_IRRI_486/Azmex_IRRI_486_contigs.db		     data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azmic_IRRI_456/Azmic_IRRI_456_contigs.db		     data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azspnov_IRRI1_472/Azspnov_IRRI1_472_contigs.db	 data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azspnov_IRRI2_489/Azspnov_IRRI2_489_contigs.db	 data/MAG_anvi_dbs/
-cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Aznil_IRRI_479/Aznil_IRRI_479_contigs.db		     data/MAG_anvi_dbs/
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_hybrid_doublefiltered_anvio/Azfil_wild/Azfil_wild_contigs.db		             \
+  data/MAG_anvi_dbs/Azfil_wild_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_hybrid_doublefiltered_anvio/Azfil_lab/Azfil_lab_contigs.db		               \
+  data/MAG_anvi_dbs/Azfil_lab_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azrub_IRRI_479/Azrub_IRRI_479_contigs.db		     \
+  data/MAG_anvi_dbs/Azrub_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azmex_IRRI_486/Azmex_IRRI_486_contigs.db		     \
+  data/MAG_anvi_dbs/Azmex_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azmic_IRRI_456/Azmic_IRRI_456_contigs.db		     \
+  data/MAG_anvi_dbs/Azmic_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azspnov_IRRI1_472/Azspnov_IRRI1_472_contigs.db	 \
+  data/MAG_anvi_dbs/Azcar1_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Azspnov_IRRI2_489/Azspnov_IRRI2_489_contigs.db	 \
+  data/MAG_anvi_dbs/Azcar2_contigs.db
+cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_singles_doublefiltered_anvio/Aznil_IRRI_479/Aznil_IRRI_479_contigs.db		     \
+  data/MAG_anvi_dbs/Aznil_contigs.db
 
 # get profile dbs and rename them
 cp --reflink=always /stor/azolla_metagenome/Azolla_genus_metagenome/data/assembly_hybrid_doublefiltered_binningsignals_anvio/MERGED_Azfil_wild/PROFILE.db		\
