12d44154bd1129ea6cad60d54848246e4cdc6ff7
laura.lwd <laura.w.dijkhuizen@gmail.com>
2022-09-05 12:44:36 +0200

    draft bash loop

--- scripts/collect_assembly_stats.bash
+++ scripts/collect_assembly_stats.bash
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo 'hoi'
+# collect nanopore assembly stats
+NANOPORE=( Azfil_lab Azpinnata Azsp_bordeaux )
+SELECTION=( Nazollae mitochondrium chloroplast )
+
+# header for the table:
+echo -e "strain\t\
+seqyield\t\
+genome\t\
+assembled_contigs\t\
+assembled_length\t\
+assembled_N50\t\
+coverage"
+
+for s in ${NANOPORE[@]}
+do  for g in ${SELECTION[@]}
+    do  strain="$s"
+        seqyield=$(grep 'Total read length' data/nanopore_assembly/$g/$s/flye.log | rev | cut -d ' ' -f 1 | rev )
+
+        echo -e "$strain\t\
+        $seqyield"
+    done
+done | tr -d ' '
+
+
+exit 0
