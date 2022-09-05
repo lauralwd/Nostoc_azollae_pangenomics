0f08786fb7255fad3bc5cbbfabb8703dfb16cd7e
laura.lwd <laura.w.dijkhuizen@gmail.com>
2022-09-05 14:15:22 +0200

    comment about required software

--- scripts/collect_assembly_stats.bash
+++ scripts/collect_assembly_stats.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# requires Bandage conda environment
+
 # collect nanopore assembly stats
 NANOPORE=( Azfil_lab Azpinnata Azsp_bordeaux )
 ILLUMINA=( Azcar1 Azcar2 Azmexicana Azmicrophylla Aznilotica Azrubra Azfil_lab )
